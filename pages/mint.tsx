05a4d0bc5d1127c5c858758a1e538bf91bc0a274
botsofcog <126983589+botsofcog@users.noreply.github.com>
2023-06-14 21:25:01 -0600

    Add files via upload

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -15,7 +15,7 @@ const Mint: NextPage = () => {
       <br></br>
       <div className={styles.optionSelectBox2}><Image src="/preview.gif" alt="preview" width={400} height={400} /></div>
       <p>
-        Early Access @10 $Matic - Public @15 $Matic
+        Early Access JULY 14-21 2023 @10 $Matic - JULY 21 2023 Public @15 $Matic
       </p>
       <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
       <iframe src="https://ipfs-2.thirdwebcdn.com/ipfs/QmZG9dPDYCpTuzM3mVvdtmpjqwCbhErPipNvT945QqzWHk?contract=0x8B9Ada84CBFBE266d103E6c90717Df789B63d0F7&chain=%7B%22name%22%3A%22Polygon+Mainnet%22%2C%22chain%22%3A%22Polygon%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fpolygon.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22MATIC%22%2C%22symbol%22%3A%22MATIC%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22matic%22%2C%22chainId%22%3A137%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22polygon%22%7D&theme=dark&primaryColor=red"
@@ -25,9 +25,13 @@ const Mint: NextPage = () => {
 <br></br>
       <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
       <p className={styles.explain}>
-        Connect Your Wallet Above To Claim Your <b>BOTS OF COG</b> Generation 2 Series Robot.
+        Connect Your Wallet Above To Claim Your <a href="https://opensea.io/collection/bots-of-cog-gen2"><b>BOTS OF COG</b> Generation 2 Series Robot</a>.
+        <br></br>
+        Bridge Your ETH to MATIC Below using the <a href="https://bridge.umbria.network/"><b>UBMBRIA Network Bridge</b></a>.
       </p>
-
+      <iframe src="https://umbria.network/widgetv2/" width="960" height="600" scrolling="no"></iframe>
+      <br></br>
+      <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
       <a href="https://www.botsofcog.io"> - BACK TO BOTS OF COG HOMEPAGE - </a> <a href="https://twitter.com/botsofcog">- BoC Twitter -</a>
     </div>
   );
