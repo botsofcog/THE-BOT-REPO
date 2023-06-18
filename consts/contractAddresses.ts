095641bbc2c5ecaf1d17eafad79cd95c9f5a9cc9
botsofcog <126983589+botsofcog@users.noreply.github.com>
2023-06-18 04:06:16 -0600

    Add files via upload

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -4,3 +4,7 @@ export const tokenContractAddress =
   "0xd82f89a16Bf2dECc0d6cb96CA12F1306fb623D42";
 export const stakingContractAddress =
   "0x6D067520526807E7A61BAC740E6D66BB62d05332";
+export const gen2DropContractAddress =
+  "0x8B9Ada84CBFBE266d103E6c90717Df789B63d0F7";
+export const gen2StakingContractAddress =
+  "0xa388f4d544bF437D8C40df85bc65A9822a0472Db";
