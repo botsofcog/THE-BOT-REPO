7c6b445f9c5b63be185c9bdb7b17403f28d298ca
botsofcog <126983589+botsofcog@users.noreply.github.com>
2023-11-25 20:23:45 -0700

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider, coinbaseWallet, metamaskWallet, rainbowWallet, walletConnect, ConnectWallet, localWallet, magicLink, trustWallet, smartWallet, } from "@thirdweb-dev/react";
+import { ThirdwebProvider, coinbaseWallet, metamaskWallet, zerionWallet, bloctoWallet, frameWallet, phantomWallet, rainbowWallet, walletConnect, ConnectWallet, localWallet, magicLink, trustWallet, smartWallet, } from "@thirdweb-dev/react";
 import { Polygon } from "@thirdweb-dev/chains";
 import "../styles/globals.css";
 import { ThirdwebNftMedia, useContract, useNFTs, useContractMetadata } from "@thirdweb-dev/react";
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ApolloProvider client={client}>
       <ThirdwebProvider
       clientId="09bddeb436c778006bbfb6c89c8bf588" 
-        supportedWallets={[coinbaseWallet(), metamaskWallet(), rainbowWallet(), walletConnect(), localWallet(),  trustWallet(), magicLink({
+        supportedWallets={[coinbaseWallet(), metamaskWallet(), zerionWallet(), bloctoWallet(), frameWallet(), phantomWallet(), rainbowWallet(), walletConnect(), localWallet(),  trustWallet(), magicLink({
           apiKey: "pk_live_FC80B0402A25F2BD",
         }),]}
         activeChain="polygon"
