5e6b7e5dcdf3390e43136d7054c375e3cc3f3248
botsofcog <126983589+botsofcog@users.noreply.github.com>
2023-12-17 18:17:26 -0700

    Add files via upload

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,10 +1,10 @@
 export const nftDropContractAddress =
   "0x1BBCa92FC889Af891e3B666aee7Cb3534B83d7B7";
 export const tokenContractAddress =
-  "0xd82f89a16Bf2dECc0d6cb96CA12F1306fb623D42";
+  "0xE0C226D7d656CF7Dd7a57597Fc808B13B17f3bdb";
 export const stakingContractAddress =
   "0x6D067520526807E7A61BAC740E6D66BB62d05332";
 export const gen2DropContractAddress =
-  "0x8B9Ada84CBFBE266d103E6c90717Df789B63d0F7";
+  "0x71eA5c257ae5dc3C0CD7867fEa30d236E2c8dF6a";
 export const gen2stakingContractAddress =
   "0xa388f4d544bF437D8C40df85bc65A9822a0472Db";
