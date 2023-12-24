69112220636dce1340865e9e40af84fee59b5ceb
botsofcog <126983589+botsofcog@users.noreply.github.com>
2023-12-24 13:37:25 -0700

    Add files via upload

---
+++ niftykit.d.ts
@@ -0,0 +1,11 @@
+// niftykit.d.ts
+
+declare module JSX {
+    interface IntrinsicElements {
+      'nk-diamond': any;
+      'nk-connect-wallet-button': any;
+      'nk-drop-mint-button': any;
+      // Add other NiftyKit elements as needed
+    }
+  }
+  
